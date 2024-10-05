92f59b13dfbbb0a57fbe88ab18a8b6a3d5176e81
reinvantveer <rein@vantveer.me>
2024-10-05 22:05:01 +0200

    add cli arguments

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+import argparse
+import random
 import tomllib
+from argparse import Namespace
 
-def main():
-    with open("pyproject.toml", "rb") as f:
+
+def main(args: Namespace) -> None:
+    with open(args.config, "rb") as f:
         data = tomllib.load(f)
 
     print(data)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser('Billy1ButtsBot service')
+    parser.add_argument('--config', default='pyproject.toml', help='Configuration')
+    parser.add_argument('--user', default='billy1buttsbot', help='User')
+    parser.add_argument('--password', help='Password', required=True)
+
+    args = parser.parse_args()
+    main(args)
